d2054b19449d90039ef19fec05f7de245ff69598
tan1234jp <ikuo.tansho@gmail.com>
2020-02-28 17:01:06 +0900

    新UI実装（実装中・未試験）

--- lib/ui/timer_chart.dart
+++ lib/ui/timer_chart.dart
@@ -3,61 +3,45 @@ import 'dart:math';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
+class GaugeSegment {
+  final String segment;
+  final int size;
+  GaugeSegment(this.segment, this.size);
+}
+
 class TimerChart extends StatelessWidget {
-  TimerChart(this.seriesList, {this.animate});
+  void setData(String title, int value) {
+    if (seriesList != null) {
+      seriesList.clear();
+    } else {
+      seriesList = List<charts.Series>();
+    }
 
-  final List<charts.Series> seriesList;
-  final bool animate;
-
-  factory TimerChart.fromValue(
-      {@required double value, @required Color color, bool animate}) {
-    return TimerChart(
-      _createDataFromValue(value, color),
-      animate: animate,
-    );
+    seriesList = [
+      charts.Series<GaugeSegment, String>(
+        id: 'Segments',
+        domainFn: (segment, _) => segment.segment,
+        measureFn: (segment, _) => segment.size,
+        data: [
+          GaugeSegment('Low', 80),
+          GaugeSegment('High', 20),
+        ],
+      )
+    ];
   }
+
+  List<charts.Series> seriesList;
 
   @override
   Widget build(BuildContext context) {
     return charts.PieChart<dynamic>(
       seriesList,
-      animate: animate,
+      animate: false,
       defaultRenderer: charts.ArcRendererConfig<dynamic>(
-        arcWidth: 20,
-        startAngle: 3 / 5 * pi,
-        arcLength: 9 / 5 * pi,
+        arcWidth: 30,
+        startAngle: 4 / 5 * pi,
+        arcLength: 7 / 5 * pi,
       ),
     );
   }
-
-  static List<charts.Series<TimerSegment, String>> _createDataFromValue(
-      double value, Color color) {
-    double toShow = (1 + value) / 2;
-    final data = [
-      TimerSegment('Main', toShow, color),
-      TimerSegment('Rest', 1 - toShow, Colors.transparent),
-    ];
-
-    return [
-      charts.Series<TimerSegment, String>(
-        id: 'Segments',
-        domainFn: (segment, _) => segment.segment,
-        measureFn: (segment, _) => segment.value,
-        colorFn: (segment, _) => segment.color,
-        labelAccessorFn: (segment, _) =>
-            segment.segment == 'Main' ? '${segment.value}' : null,
-        data: data,
-      )
-    ];
-  }
-}
-
-class TimerSegment {
-  TimerSegment(this.segment, this.value, Color color)
-      : this.color = charts.Color(
-            r: color.red, g: color.green, b: color.blue, a: color.alpha);
-
-  final String segment;
-  final double value;
-  final charts.Color color;
 }
